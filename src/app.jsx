d1fdff24562bb80d159f65f4bf7b9b3b7573edd1
Patrick Werner <pwerner2@gmail.com>
2017-04-22 11:58:53 -0400

    console logging user input

--- src/app.jsx
+++ src/app.jsx
@@ -2,10 +2,27 @@ import React, { Component } from 'react';
 import './app.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      deadline: 'December 24, 2018',
+      newDeadline: ''
+    }
+  }
+  //in react, state is always an object. The argument 'props' is also convention.
+
+  changeDeadline(){
+    this.setState({
+      deadline: 'November 25, 2018'
+    })
+  }
+
   render(){
     return (
       <div className="App">
-        <div className="appTitle">Coundown: Time Until X</div>
+        <div className="appTitle">
+           Coundown: Time Until {this.state.deadline}
+        </div>
         <div>
             <div className="clockDays">14 Days</div>
             <div className="clockHours">30 Hours</div>
@@ -13,13 +30,19 @@ class App extends Component {
             <div className="clockSeconds">30 Seconds</div>
         </div>
         <div>
-            <input placeholder='target date'/>
-            <button>Submit</button>
+            <input
+              placeholder='target date'
+              onChange={event => console.log(event.target.value, 'event')}
+            />
+            <button onClick={() => this.changeDeadline()}>
+              Submit
+            </button>
         </div>
       </div>
     )
   }
 }
 
+// () => this.changeDeadline == ES6 Anonymous Function
 //html markup that is rendered must also be return()ed in react
 export default App;
