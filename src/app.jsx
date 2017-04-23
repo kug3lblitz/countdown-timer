704c9721396ab62987d76827facc6d805516a318
Patrick Werner <pwerner2@gmail.com>
2017-04-22 21:29:58 -0400

    basic styles with bootstrap, appended a '0' to single digit values. Getting errors related to forthcoming react version, related to react-bootstrap

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Clock from './clock.jsx';
 import './app.css';
+import { Form, FormControl, Button } from 'react-bootstrap';
 
 class App extends Component {
   constructor(props) {
@@ -25,15 +26,16 @@ class App extends Component {
         <Clock
           deadline={this.state.deadline}
         />
-        <div>
-            <input
+        <Form inline>
+            <FormControl
+              className="deadlineInput"
               placeholder='target date'
               onChange={event => this.setState({newDeadline: event.target.value})}
             />
-            <button onClick={() => this.changeDeadline()}>
+            <Button onClick={() => this.changeDeadline()}>
               Submit
-            </button>
-        </div>
+            </Button>
+        </Form>
       </div>
     )
   }
--- src/clock.jsx
+++ src/clock.jsx
@@ -20,6 +20,17 @@ class Clock extends Component {
     setInterval(() => this.getTimeUntil(this.props.deadline), 1000);
   }
 
+  //leading0(num) {
+    //if (num < 10) {
+      //return '0' + num;
+    //}
+    //return num;
+  //}
+
+  leading0(num) {
+    return num < 10 ? '0' + num : num;
+  }
+
   getTimeUntil(deadline) {
     const time = Date.parse(deadline) - Date.parse(new Date());
     const seconds = Math.floor((time/1000)%60);
@@ -36,10 +47,10 @@ class Clock extends Component {
   render() {
     return (
         <div>
-            <div className="clockDays">{this.state.days} Days</div>
-            <div className="clockHours">{this.state.hours} Hours</div>
-            <div className="clockMinutes">{this.state.minutes} Minutes</div>
-            <div className="clockSeconds">{this.state.seconds} Seconds</div>
+            <div className="clockDays">{this.leading0(this.state.days)} Days</div>
+            <div className="clockHours">{this.leading0(this.state.hours)} Hours</div>
+            <div className="clockMinutes">{this.leading0(this.state.minutes)} Minutes</div>
+            <div className="clockSeconds">{this.leading0(this.state.seconds)} Seconds</div>
         </div>
     )
   }
